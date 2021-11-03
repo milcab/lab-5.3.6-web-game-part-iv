122e9948602f8dd11a4ece1c6b869f9fd51c5b5e
milcab <milcarodriguez23@gmail.com>
2021-11-03 01:03:16 -0400

    make character move with arrow keys

--- move.js
+++ move.js
@@ -6,7 +6,73 @@ function move(element) {
         element.style.bottom = bottom + 'px'
     }
 
+    function withArrowKeys(left, bottom, callback) {
+        let direction = null;
+
+        let x = left
+        let y = bottom
+
+        function moveElement() {
+            let maxTop = window.innerHeight - element.height
+            if (direction === 'north' && y < maxTop) {
+                y++
+            }
+
+            const maxRight = window.innerWidth - element.width
+            if (direction === 'east' && x < maxRight) {
+                x++
+            }
+
+            if (direction === 'south' && y > 0) {
+                y--
+            }
+
+            if (direction === 'west' && x > 0) {
+                x--
+            }
+
+            move(element).to(x, y)
+        }
+
+
+        setInterval(moveElement, 1)
+
+
+        document.addEventListener('keydown', function (e) {
+            if (e.repeat) return
+
+            if (e.key === 'ArrowUp') {
+                direction = 'north'
+            }
+
+            if (e.key === 'ArrowRight') {
+                direction = 'east'
+            }
+
+            if (e.key === 'ArrowDown') {
+                direction = 'south'
+            }
+
+            if (e.key === 'ArrowLeft') {
+                direction = 'west'
+            }
+
+            if (callback) {
+                callback(direction)
+            }
+
+        })
+
+        document.addEventListener('keyup', function (e) {
+            direction = null
+            if (callback) {
+                callback(direction)
+            }
+        })
+    }
+
     return {
-        to: moveToCoordinates
+        to: moveToCoordinates,
+        withArrowKeys
     }
 }
